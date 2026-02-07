a6f3e34461d5a11e81e094c202a248411c97ac90
AlphaAhmad <muhammadahmad8063@gmail.com>
2026-02-07 15:25:57 +0500

    minds-behind-mission-and-we-are-hiring-banner-and-caledly-section-done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,9 @@ import Footer from "@/components/layout/Footer";
 import AboutHeroSection from "@/components/about/AboutHeroSection";
 import TrustedBy from "@/components/sections/TrustedBy";
 import OurValues from "@/components/about/OurValues";
+import TeamSection from "@/components/sections/TeamSection";
+import HiringBanner from "@/components/sections/HiringBanner";
+import BookingSection from "@/components/home/BookingSection";
 
 export default function AboutPage() {
   return (
@@ -12,6 +15,9 @@ export default function AboutPage() {
       <AboutHeroSection />
       <TrustedBy />
       <OurValues />
+      <TeamSection />
+      <HiringBanner />
+      <BookingSection />
 
       <Footer />
     </div>
